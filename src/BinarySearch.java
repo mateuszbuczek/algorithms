66251aa124508fbb1ec802662a6e40cc92feb5e7
Mateusz Buczek <this.mateuszbuczek@gmail.com>
2019-03-02 12:29:37 +0100

    binarySearch

---
+++ src/BinarySearch.java
@@ -0,0 +1,12 @@
+public class BinarySearch {
+    int binarySearch(int arr[], int l, int r, int x) {
+        if(r >= l) {
+            int mid = l + (r - 1)/2;
+
+            if(arr[mid] == x) return mid;
+            if(arr[mid] > x) binarySearch(arr, l, mid - 1, x);
+            return binarySearch(arr, mid + 1, r, x);
+        }
+        return -1;
+    }
+}
