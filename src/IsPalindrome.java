2cee1ddff0e2cd079a9ff84d2b2942a09e0fab33
Mateusz Buczek <this.mateuszbuczek@gmail.com>
2019-03-03 11:42:00 +0100

    isPalindrome

---
+++ src/IsPalindrome.java
@@ -0,0 +1,27 @@
+public class IsPalindrome {
+    boolean isPalindrome1(int x) {
+        if(x < 0) return false;
+
+        int rev = 0;
+
+        while( x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            rev = rev * 10 + pop;
+        }
+        return rev == x;
+    }
+
+    boolean isPalindrome2(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int revertedNumber = 0;
+        while( x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return revertedNumber == x || revertedNumber/10 == x;
+    }
+}
