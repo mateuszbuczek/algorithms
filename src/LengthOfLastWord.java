9d084c843fd83cce1a503cdaa778855bf21f382a
Mateusz Buczek <this.mateuszbuczek@gmail.com>
2019-03-19 12:20:33 +0100

    LengthOfLastWord

---
+++ src/LengthOfLastWord.java
@@ -0,0 +1,17 @@
+public class LengthOfLastWord {
+    public static int lengthOfLastWord(String s) {
+        String[] arr = s.split(" ");
+        if(arr.length < 2) return 0;
+        return arr[arr.length-1].length();
+    }
+
+    public static int lengthOfLastWord1(String s) {
+        s = s.trim();
+        int lastIndex = s.lastIndexOf(' ') + 1;
+        return s.length() - lastIndex;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord1(" wer we wergasdf"));
+    }
+}
