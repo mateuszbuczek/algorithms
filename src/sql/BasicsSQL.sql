5668af4e72e7f29f5db7b2f893ddb8541a88a543
mateuszbuczek <mateuszbuczek.128@gmail.com>
2019-06-22 12:47:52 +0200

    update

--- src/sql/BasicsSQL.sql
+++ src/sql/BasicsSQL.sql
@@ -1,3 +1,5 @@
+SELECT
+
 SELECT name, population FROM world
 WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
@@ -12,3 +14,28 @@ SELECT name, capital
 FROM world
 WHERE LEFT(name,1) = LEFT(capital,1)
 AND name <> capital
+
+-- Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.
+SELECT winner, subject
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY  subject IN ('Physics','Chemistry') ,subject, winner
+
+SELECT IN SELECT
+
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
+
+--       Which countries have a GDP greater than every country in Europe
+SELECT name FROM world
+WHERE gdp > ALL(SELECT gdp FROM world WHERE continent = 'Europe' AND gdp > 0)
+7.
+-- Find the largest country (by area) in each continent
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND population>0)
+
