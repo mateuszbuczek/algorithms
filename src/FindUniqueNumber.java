f3a439c5a88c34526c5275684e2832d61e1d1a3b
mateusz <mateuszbuczek.128@gmail.com>
2019-07-04 18:55:43 +0200

    initial

---
+++ src/FindUniqueNumber.java
@@ -0,0 +1,25 @@
+package src;
+
+//There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+public class FindUniqueNumber {
+
+    public static double findUniq(double arr[]) {
+        double first = arr[0];
+        double second = arr[1];
+
+        if(first != second)
+            return arr[2] == first ? second : first;
+
+        for (int i = 2; i < arr.length; i++) {
+
+            if(arr[i] != first)
+                return arr[i];
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findUniq(new double[] { 0, 0, 0.55, 0, 0}));
+    }
+}
