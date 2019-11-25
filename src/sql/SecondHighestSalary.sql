0690192f7443afea798ac26897c53936aabaeb9c
Mateusz Buczek <mateuszbuczek.128@gmail.com>
2019-11-25 08:12:31 +0100

    another way to get second highest salary

--- src/sql/SecondHighestSalary.sql
+++ src/sql/SecondHighestSalary.sql
@@ -1,1 +1,3 @@
 SELECT MAX(A.salary) as SecondHighestSalary FROM employee A join employee B on A.salary < B.salary and A.id != B.id
+
+SELECT (SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1 OFFSET 1) AS SecondHighestSalary;
